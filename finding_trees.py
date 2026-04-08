3d8ddb2bbf44edcf2098f1fbfb72932a7aa6d0b1
FlorianMeijer <f.o.v.meijer@student.utwente.nl>
2026-04-08 18:47:04 +0200

    finding_trees.py
    - Possibly solved the #Aut problem for trees

--- finding_trees.py
+++ finding_trees.py
@@ -1,6 +1,6 @@
 from graph import *
 from graph_io import *
-
+import math
 
 def count_tree_isomorphisms(graphs:list[Graph]):
     """
@@ -15,10 +15,11 @@ def count_tree_isomorphisms(graphs:list[Graph]):
     if len(root_G)!= len(root_H):
         return 0
     AHU_codes_G, AHU_codes_H = sorted([AHU(r) for r in root_G]), sorted([AHU(r) for r in root_H])
+    print(AHU_codes_G, AHU_codes_H)
     if AHU_codes_G != AHU_codes_H:
         return 0
     if len(root_G)==2:
-        if AHU_codes_G[0]==AHU_codes_H[1]:
+        if AHU_codes_G[0]==AHU_codes_H[0]:
             return number_of_AHUmorphisms(AHU_codes_G[0]) * 2
         else:
             return number_of_AHUmorphisms(AHU_codes_G[0])
@@ -27,25 +28,57 @@ def count_tree_isomorphisms(graphs:list[Graph]):
 
 
 def number_of_AHUmorphisms(AHU_code:str):
-    if AHU_code == "0":
-        return 1
-    return None
+    """
+    Finding |Aut| for a AHU label.
 
+    Input: AHU_code represented by str
+    Output: int
+        representing the |Aut|
+    """
+    inside = AHU_code[1:-1]
+    if AHU_code == "0" or inside == "0":
+        return 1
+    subtrees, balance, start_index = [], 0, 0
+    for i, char in enumerate(inside):
+        if char == "0" and balance == 0:
+            subtrees.append(inside[start_index:i+1])
+            start_index = i+1
+        elif char == "(":
+            balance += 1
+        elif char == ")":
+            balance -= 1
+            if balance == 0:
+                subtrees.append(inside[start_index:i+1])
+                start_index = i+1
+    total_aut = 1
+    for substr, n in counting_subtrees(subtrees).items():
+        internal_aut = number_of_AHUmorphisms(substr)
+        total_aut *= (internal_aut**n)*math.factorial(n)
+    return total_aut
+
+def counting_subtrees(subtrees = list[str]):
+    counts = {}
+    for s in subtrees:
+        if s in counts:
+            counts[s] += 1
+        else:
+            counts[s] = 1
+    return counts
 
 def AHU(u:Vertex, parent:Vertex=None):
     """
-    Finding the unique label of the root of a rooted Tree.
+    Finding the unique AHU label from the root of a rooted Tree.
 
     Input: Vertex (that is the root of a rooted Tree)
     Output: list
         Contains either one or two vertices depending on the size of the graph.
     """
     result = []
-    if u.degree == 1:
-        return "0"
     for v in u.neighbours:
         if v!=parent:
             result.append(AHU(v,u))
+    if not result:
+        return "0"
     return "(" + "".join(sorted(result)) + ")"
 
 
@@ -58,7 +91,7 @@ def finding_root_tree(G:Graph):
         Contains either one or two roots depending on the size of the graph.
     """
     current_degree = {v:v.degree for v in G.vertices}
-    queue = [v for v in G.vertices if current_degree[v]==1]
+    queue = [v for v in G.vertices if current_degree[v]==1 or current_degree[v]==0]
     remaining_vertices = len(G.vertices)
     while remaining_vertices >2:
         remaining_vertices -= len(queue)
